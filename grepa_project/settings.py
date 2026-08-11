d5b1f0fe16b45775fa5a2f9d4c9607882454b81b
Mbalenhle Hadebe <mbalirhhadebe@gmail.com>
2026-08-11 15:15:40 +0200

    Add gunicorn to requirements

--- grepa_project/settings.py
+++ grepa_project/settings.py
@@ -117,10 +117,14 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/styles/'
-STATICFILES_DIRS = [BASE_DIR / "styles"]
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "styles",
+]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
